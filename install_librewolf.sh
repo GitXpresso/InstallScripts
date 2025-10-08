c608481d6757bd09a2c9b070c00eb1d137fdbf00
William <126926699+GitXpresso@users.noreply.github.com>
2025-10-08 09:20:46 -0400

    Create install_librewolf.sh

---
+++ install_librewolf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if grep -qi "Ubuntu" /etc/*release && if grep -qi "Debian" /etc/*release then
+  echo "you are using debian, running script..."
+else
+  exit 1
+fi
+if ! -f /var/lib/apt/list/.deb*$(arch)*; then
+  if $(id -u root) -ne 0; then
+    apt update
+  else
+    sudo apt update
+  fi
+fi
+if ! dpkg -l | grep -qw extrepo; then
+  sudo apt install extrepo -y
+  check_if_disabled=$(sudo extrepo list | grep -qi librewolf)
+  if ! $check_if_disabled | grep -qi disabled; then
+    sudo extrepo enable librewolf
+  fi
+else
+  echo "Extrepo already installed."
+fi
+if ! ls /etc/apt/sources.list.d/ | grep -qi librewolf &>/dev/null; then
+  echo "librewolf extrepo source was not added, updating..."
+  sudo apt update
+fi
+echo "installing librewolf..."
+sudo apt install -y librewolf
+clear
+echo "Librewolf is now installed on your system"
